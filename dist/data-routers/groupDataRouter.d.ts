31b63c2c3728bbbb38e8e0caec7af5d5ab1a1dc8
Kaelan Smith <kaelancsmith@gmail.com>
2024-02-20 14:16:22 -0800

    add groupDataRouter

---
+++ dist/data-routers/groupDataRouter.d.ts
@@ -0,0 +1,3 @@
+import { type WPDataRouter } from "cloakwp/blocks";
+import type { ContainerProps } from "@cloakui/types";
+export declare const groupDataRouter: WPDataRouter<ContainerProps>;
---
+++ dist/data-routers/groupDataRouter.js
@@ -0,0 +1,22 @@
+import { wpBlockStyleBuilder } from "cloakwp/blocks";
+import { cx } from "@cloakui/styles";
+export const groupDataRouter = (block, blockRenderer) => {
+    const { classes, styles } = wpBlockStyleBuilder(block);
+    const { innerBlocks, attrs: { gradient, tagName } = {} } = block;
+    const children = blockRenderer.render(innerBlocks, {
+        parent: block,
+    });
+    let customClasses;
+    if (gradient) {
+        customClasses = {
+            "root-to-root-dim": "bg-gradient-to-b from-root from-50% via-root-muted via-50% to-root-dim to-50%",
+            "root-dim-to-root": "bg-gradient-to-b from-root-dim from-50% via-root-muted via-50% to-root to-50%",
+        }[gradient];
+    }
+    return {
+        as: tagName,
+        className: cx("bg-root", classes, customClasses),
+        style: styles,
+        children,
+    };
+};
